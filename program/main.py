cc72b910c21fcc83684cc4affef47e46b5b6432d
NA33AT0R <Gabrielllarsen@outlook.com>
2024-11-21 11:25:23 +0100

    Added print for average hop delay

--- program/main.py
+++ program/main.py
@@ -4,25 +4,33 @@ from flow import Flow
 
 
 def main():
-    net = Network("../simulation_files/little/streams.csv",
-                  "../simulation_files/little/topology.csv")
+    # net = Network("../simulation_files/little/streams.csv",
+    #               "../simulation_files/little/topology.csv")
+    
+    net = Network("../test_cases/small-streams.csv",
+                  "../test_cases/small-topology.csv")
 
     flows = []
     for stream in net.streams.iterrows():
         flow = Flow(stream[1])
         flow.find_path(net.G)
         flows.append(flow)
-    
+
     for flow in flows:
         flow.fill_nodes()
 
     for flow in flows:
         flow.get_total_delay()
         print(flow)
-
+        
     # for flow in flows:
     #     flow.new_get_total_delay()
     #     print(flow)
-
+    
+    Counter=0
+    for flow in flows:
+        Counter+=flow.total_delay
+    print("Average Flow %f" % (Counter/len(flows)))
+    
 if __name__ == "__main__":
     main()
